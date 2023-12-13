8271a7fa3e36e478156069d22d396371696feafb
fernaldy <76620982+fernaldya@users.noreply.github.com>
2023-12-13 11:52:39 +0700

    added get_time function for consistent timestamps

--- src/scrape.py
+++ src/scrape.py
@@ -5,6 +5,9 @@ from bs4 import BeautifulSoup as bs
 from datetime import datetime
 from time import sleep
 
+def get_time():
+    return datetime.now().strftime('%H-%M-%S_%Y-%m-%d')
+
 def scrape(url, driver):
     """
     Function to scrape data with the parameters:
@@ -16,7 +19,7 @@ def scrape(url, driver):
     
     If the scrape is sucessful (table not empty) the function outputs all the table rows <tr>
     """
-    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    current_time = get_time()
     print('Web scraped at: ', current_time)
     
     max_attempts = 5
@@ -39,7 +42,7 @@ def scrape(url, driver):
             driver.quit()
             tbody = table.find('tbody') # Fetches the table body
             table_rows = tbody.find_all('tr') # Fetches all the table rows
-            return table_rows
+            return table_rows, current_time
         sleep(3)
         
     if not table:
